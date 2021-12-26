35ecbf6abcfa84165428976eb83a4aa7d46ac292
Michael Mesquita <mannolaur@gmail.com>
2021-12-25 21:51:16 -0500

    Moved application information into info.py file.

---
+++ src/info.py
@@ -0,0 +1,12 @@
+# Info
+
+"""Project information including author, copyright, license, and version."""
+name = "Malicious URL Checker Bot"
+author = "Michael Mesquita (Midlight25)"
+copyright = "Copyright 2021, Michael Mesquita"
+
+license = "MIT"
+version = "0.0.1"
+maintainer = "Michael Mesquita"
+email = "mesquitamichael000@gmail.com"
+status = "Development"
